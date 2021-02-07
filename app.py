19c85b52981e108ba911fc00a559bf8a9a6fcdaa
Nick Horton <nhorton04@gmail.com>
2021-02-07 00:27:24 -0800

    init commit

---
+++ app.py
@@ -0,0 +1,44 @@
+from test_on_image import main
+import streamlit as st
+from models import TransformerNet
+from utils import *
+import torch
+from torch.autograd import Variable
+import argparse
+import os
+import tqdm
+from torchvision.utils import save_image
+from PIL import Image
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--image_path", default='./aligned_images', type=str, required=True, help="Path to image")
+    parser.add_argument("--checkpoint_model", default='./models/starry_night10000', type=str, required=True, help="Path to checkpoint model")
+    args = parser.parse_args()
+    print(args)
+
+    os.makedirs("images/outputs", exist_ok=True)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    transform = style_transform()
+
+    # Define model and load model checkpoint
+    transformer = TransformerNet().to(device)
+    transformer.load_state_dict(torch.load(args.checkpoint_model))
+    transformer.eval()
+
+    # Prepare input
+    image_tensor = Variable(transform(Image.open(args.image_path))).to(device)
+    image_tensor = image_tensor.unsqueeze(0)
+
+    # Stylize image
+    with torch.no_grad():
+        stylized_image = denormalize(transformer(image_tensor)).cpu()
+
+    # Save image
+    fn = args.image_path.split("/")[-1]
+    save_image(stylized_image, f"images/outputs/stylized-{fn}")
+
+if __name__ == "__main__":
+    main()
